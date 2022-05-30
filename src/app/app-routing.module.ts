b439a36f7d66717c33fe1da699acee5fb71b6a39
Wesley DuSell <dusellfam@gmail.com>
2022-05-30 10:06:52 -0400

    Create 404 module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'Home' },
   {
-    path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
-  }
+    path: '404',
+    loadChildren: () => import('./pages/not-found/not-found.module').then(m => m.NotFoundModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
+  },
+  { path: '**', redirectTo: '404' }
 ];
 
 @NgModule({
